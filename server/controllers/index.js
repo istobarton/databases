d1aa2ce740e229760363fbcb7e6a86e335341306
istobarton <istobarton@gmail.com>
2015-07-09 20:29:38 -0500

    Implemented the beginning of Sequelize ORM functionality

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,36 @@
 var models = require('../models');
 var bluebird = require('bluebird');
+var orm = require('../db');
 
 
 
 module.exports = {
   messages: {
     get: function (req, res) { // a function which handles a get request for all messages
-      models.messages.get(req, res);
+      orm.Message.findAll({ include: [orm.User] })
+        .then(function(results){
+          var output = results.map(function(res){
+            return {text: res.getDataValue('text'), username: res.getDataValue('username'), roomname: res.getDataValue('roomname')};
+          })
+          console.log(JSON.stringify(output)+ "____****____*******_______")
+          res.json({results: output});
+        });
+
+      //models.messages.get(req, res);
     },
     post: function (req, res) {
-      models.messages.post(req, res);
+      orm.User.findOrCreate({where:{username: req.body.username}})
+        .then(function(user){
+          var params = {
+            text: req.body.text,
+            UserId: user[0].dataValues.id,
+            roomname: req.body.roomname
+          };
+          orm.Message.create(params).then(function(){
+            res.sendStatus(201);
+          });
+        });
+      // models.messages.post(req, res);
     } // a function which handles posting a message to the database
   },
 
--- server/db/index.js
+++ server/db/index.js
@@ -1,41 +1,41 @@
-// var Sequelize = require('Sequelize');
-// var orm = new Sequelize('chat', 'root', '', {host:'localhost', dialect: 'mysql'});
+var Sequelize = require('Sequelize');
+var orm = new Sequelize('chat', 'root', '', {host:'localhost', dialect: 'mysql', define:{timestamps: false}});
 
 
 
-// var User = orm.define('User', {
-//   username: Sequelize.STRING
-// });
-
-// var Message = orm.define('Message', {
-//   text: Sequelize.STRING,
-//   roomname: Sequelize.STRING
-// });
-
-// User.hasMany(Message);
-// Message.belongsTo(User);
-
-// User.sync();
-// Message.sync();
-
-// exports.User = User;
-// exports.Message = Message;
-
-var mysql = require('mysql');
-
-// Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
-// and to the database "chat".
-
-module.exports = mysql.createConnection({
-  // host:'localhost',
-  user: 'root',
-  password: '',
-  database: 'chat'
+var User = orm.define('User', {
+  username: Sequelize.STRING
 });
 
-module.exports.connect(function(err){
-  if(err) { console.error(err); }
+var Message = orm.define('Message', {
+  text: Sequelize.STRING,
+  roomname: Sequelize.STRING
 });
+
+User.hasMany(Message);
+Message.belongsTo(User);
+
+User.sync();
+Message.sync();
+
+exports.User = User;
+exports.Message = Message;
+
+// var mysql = require('mysql');
+
+// // Create a database connection and export it from this file.
+// // You will need to connect with the user "root", no password,
+// // and to the database "chat".
+
+// module.exports = mysql.createConnection({
+//   // host:'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'chat'
+// });
+
+// module.exports.connect(function(err){
+//   if(err) { console.error(err); }
+// });
 
 
